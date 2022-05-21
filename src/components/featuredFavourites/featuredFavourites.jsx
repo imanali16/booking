7a691ec507c3a4e401a45a9eced8df25b57d8686
imanali16 <thevexare@gmail.com>
2022-05-21 20:00:49 +0300

    first commit

---
+++ src/components/featuredFavourites/featuredFavourites.jsx
@@ -0,0 +1,59 @@
+import "./featuredFavourites.css";
+
+const FeaturedFavourites = () => {
+    return (
+        <div className="fp">
+            <div className="fpItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="fpImg"/>
+                <span className="fpName">Apartments Bone</span>
+                <span className="fpCity">Valencia</span>
+                <span className="fpPrice">Starting from 100$</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="fpImg"/>
+                <span className="fpName">Apartments Bone</span>
+                <span className="fpCity">Valencia</span>
+                <span className="fpPrice">Starting from 100$</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="fpImg"/>
+                <span className="fpName">Apartments Bone</span>
+                <span className="fpCity">Valencia</span>
+                <span className="fpPrice">Starting from 100$</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="fpImg"/>
+                <span className="fpName">Apartments Bone</span>
+                <span className="fpCity">Valencia</span>
+                <span className="fpPrice">Starting from 100$</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default FeaturedFavourites
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,56 @@
+import "./propertyList.css";
+
+const PropertyList = () => {
+    return (
+        <div className="pList">
+            <div className="pListItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="pListImg"/>
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>12321 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="pListImg"/>
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>12321 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="pListImg"/>
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>12321 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="pListImg"/>
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>12321 hotels</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img
+                    src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+                    alt="" className="pListImg"/>
+                <div className="pListTitles">
+                    <h1>Hotels</h1>
+                    <h2>12321 hotels</h2>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default PropertyList
